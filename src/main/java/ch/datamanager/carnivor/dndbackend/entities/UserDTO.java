1f05ad58955688c5c83cdfddc4bf7489ba5607b2
C4RN1V0R <lumawa@gmx.ch>
2022-10-13 14:11:23 +0200

    add id and mail to DTO

--- src/main/java/ch/datamanager/carnivor/dndbackend/entities/UserDTO.java
+++ src/main/java/ch/datamanager/carnivor/dndbackend/entities/UserDTO.java
@@ -10,13 +10,20 @@ import java.util.List;
 public class UserDTO {
 
     private final String username;
+    private final String id;
+    private final String mail;
 
-    private UserDTO(String username){
+    private UserDTO(String id, String username, String mail){
         this.username = username;
+        this.id = id;
+        this.mail = mail;
     }
 
     public static UserDTO build(User user){
-        return new UserDTO(user.getUsername());
+        String id = user.getId().toString();
+        String username = user.getUsername();
+        String mail = user.getMail();
+        return new UserDTO(id, username, mail);
     }
 
     public static List<UserDTO> build(Iterable<User> users){
